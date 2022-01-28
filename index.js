bcdb2d5e02178a8a0d12af588f401630515785f4
NurulIslam-EEE <ni163034@gmail.com>
2022-01-28 22:48:33 +0600

    update status

--- index.js
+++ index.js
@@ -243,6 +243,20 @@ async function run() {
             console.log('updating', id)
             res.json(result)
         })
+        //update review status 
+        app.put('/blogStatus/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedStatus = req.body;
+            const query = { _id: ObjectId(id) };
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    status: updatedStatus.status
+                },
+            };
+            const result = await blogsCollection.updateOne(query, updatedDoc, options)
+            res.json(result)
+        })
         //post api for add review insert
         app.post("/review", async (req, res) => {
             const review = req.body;
